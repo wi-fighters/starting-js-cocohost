daab48d01979f61e651cc45e28ef1fc8f91a7d52
cocohost <kontakt.nicolas@gmail.com>
2019-08-12 15:50:32 +0200

    added variables city, job, hobby

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let firstName = "Green"; // expression
 let lastName = "Banana"; // declaration and assignment
+let city = "Monkey City"
+let job = "Uber Coder"
+let hobby = "Making Origami from Banana leafs"
 
 console.log("Hello " + firstName + " " + lastName); // statement
