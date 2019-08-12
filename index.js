98c67d56c175c15a68a38d88a5727eb8f4463c04
cocohost <kontakt.nicolas@gmail.com>
2019-08-12 16:06:09 +0200

    added variables of Data Types

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let city = "Berlin";
 let age = 66;
 let job = "Coder";
 let hobby = "Making Origami from Banana leafs";
+let length = 16.78; 
+let lastName = "Johnson"; 
+let car = ""; 
+let isOpen = true;
 
 console.log(firstName + " " + lastName + " " + "is a" + " " + age + " " + "year old" + " "
 + job +  " " + "who lives in" + " " + city); // statement
