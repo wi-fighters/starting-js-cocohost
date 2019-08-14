9ecf61d4d63bca3c615722c39e119ee3c7e6e5a8
cocohost <kontakt.nicolas@gmail.com>
2019-08-14 09:55:19 +0200

    create structure of index.js

---
+++ ternaryOperators/index.js
@@ -0,0 +1,21 @@
+// 1. canDrive?
+
+const canDrive = 18;
+const Paul = 33;
+const Peter = 17;
+
+
+
+
+// 2. Slow Down!
+
+
+// 3. How old are you?
+
+
+// 4. Student Discount
+
+
+// 5. What time is it?
+
+
