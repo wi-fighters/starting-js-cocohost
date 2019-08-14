7586383eafa07dacdb6210c0f6bc522ea5b31bc7
cocohost <kontakt.nicolas@gmail.com>
2019-08-14 16:03:26 +0200

    solved exercise 1

---
+++ stringMethods/index.js
@@ -0,0 +1,20 @@
+// 1. Characters & Properties
+// Declare a variable and assign a string as a value. 
+// Make sure that the string is at least six characters long. 
+//Print the length to the console. Access and print the 2nd and 6th elements of the string. 
+
+const myName = "Nicolas";
+console.log(`Name length is ${name.length}`);
+const second = myName[1];
+const sixth = myName[5];
+
+console.log(`The 2nd element is ${second}, the 6th element is ${sixth}`);
+
+
+
+
+
+// 2. JS 
+// Declare a variable and assign the value `JavaScript` to the variable. 
+// Print the length of the string to the console, then print the `J` & `S` characters to the console.
+
