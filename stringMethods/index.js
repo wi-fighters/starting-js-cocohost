6491792a1200ebe7754796d9d3d40765225bb02a
cocohost <kontakt.nicolas@gmail.com>
2019-08-14 16:12:07 +0200

    next quest 3

--- stringMethods/index.js
+++ stringMethods/index.js
@@ -4,17 +4,17 @@
 //Print the length to the console. Access and print the 2nd and 6th elements of the string. 
 
 const myName = "Nicolas";
-console.log(`Name length is ${name.length}`);
+console.log(`Name length is ${myName.length}`);
 const second = myName[1];
 const sixth = myName[5];
 
 console.log(`The 2nd element is ${second}, the 6th element is ${sixth}`);
 
-
-
-
-
 // 2. JS 
 // Declare a variable and assign the value `JavaScript` to the variable. 
 // Print the length of the string to the console, then print the `J` & `S` characters to the console.
+
+const language = "JavaScript";
+console.log(`Name length is ${language.length}`);
+console.log(language[0] , language[4] );
 
