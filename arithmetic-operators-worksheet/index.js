34557ae9815bfeb4051ff802db969c1fcc71bb64
cocohost <kontakt.nicolas@gmail.com>
2019-08-14 09:40:14 +0200

    Added comments as structure in index.js

---
+++ arithmetic-operators-worksheet/index.js
@@ -0,0 +1,32 @@
+// Math Problems
+
+// 1.Percent
+
+// 2. Working with Averages
+
+// 2.1 Working Hours
+
+
+// 2.3 Score
+
+// 2.4 The Minimum Score
+
+// 3. Consumer Formula
+
+// 4. Geometry Formulas
+
+// 4.1 Perimeter of a Square
+
+// 4.2 Perimeter of a Triangle
+
+// 4.3 Area of a Square
+
+// 4.4 Area of a Triangle
+
+// 4.5 Volume of a Cube
+
+// 5. Basic Word Problems
+
+// 5.1 At the movies
+
+// 5.2 Income per Week
