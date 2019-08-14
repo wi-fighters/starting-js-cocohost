72931a9696b1ad7f771dbaf63c2bcbc26c0c99ac
cocohost <kontakt.nicolas@gmail.com>
2019-08-14 10:14:05 +0200

    solved number 2

--- ternaryOperators/index.js
+++ ternaryOperators/index.js
@@ -1,4 +1,7 @@
 // 1. canDrive?
+// Declare a variable named canDrive. Check if someone's age is over 18. If so, 
+// print `Yes, you can drive! Hope you have a license` 
+// and if not, print `Nope, you're too young!`
 
 const canDrive = 18;
 const personAge = 33;
@@ -7,7 +10,15 @@ const ageCheck = (personAge >= canDrive) ? "Yes, you can drive! Hope you have a 
 console.log(ageCheck);
 
 // 2. Slow Down!
+// Declare a variable named speedCheck. If speed is above 130km, 
+// print `You're going too fast - slow down!`. 
+// If speed is lower than 130km, print `You're driving below the speed limit`.
 
+const speedZone = 130
+const speedTest = 200
+
+const speedCheck = (speedTest >= speedZone) ? "You're going too fast - slow down!" : "You're driving below the speed limit";
+console.log(speedCheck);
 
 // 3. How old are you?
 
