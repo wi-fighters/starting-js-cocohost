5cb0e4fd5aa7ca3dbc5b73270d3a44ed98a4a048
cocohost <kontakt.nicolas@gmail.com>
2019-08-20 15:49:18 +0200

    addinf exercise

---
+++ name-analyzer/index.js
@@ -0,0 +1,6 @@
+// Name Analyzer
+const name = "Winston";
+console.log(`Hello Winston.
+Your name is ${name.length() - 1} letters long.
+The first letter of your name is W, the last is n.
+If I wanted to be funny, I would call you Winschmoo.`);
