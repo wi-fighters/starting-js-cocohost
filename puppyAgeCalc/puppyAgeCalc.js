0c356a6b87f400d8744d127e71d87b747c8e961e
cocohost <kontakt.nicolas@gmail.com>
2019-08-27 13:31:18 +0200

    added humanAge parameter

--- puppyAgeCalc/puppyAgeCalc.js
+++ puppyAgeCalc/puppyAgeCalc.js
@@ -1,6 +1,8 @@
-function calculateDogAge(puppyAge) {
-  return `Ỳour doggie is ${puppyAge * 7} years old in dog years!`;
+function calculateDogAge(humanAge) {
+  const puppyAge = humanAge * 7;
+
+  return `Ỳour doggie is ${puppyAge} years old in dog years!`;
 }
-console.log(calculateDogAge(3));
-console.log(calculateDogAge(5));
-console.log(calculateDogAge(7));
+console.log(calculateDogAge(31));
+console.log(calculateDogAge(52));
+console.log(calculateDogAge(30));
