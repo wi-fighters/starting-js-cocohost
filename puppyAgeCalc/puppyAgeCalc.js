339a40dc12127a090337ba84d4cb102c83642aeb
cocohost <kontakt.nicolas@gmail.com>
2019-08-27 13:24:57 +0200

    addedpuppyAgeCalc.js

---
+++ puppyAgeCalc/puppyAgeCalc.js
@@ -0,0 +1,6 @@
+function calculateDogAge(puppyAge) {
+  return `Ỳour doggie is ${puppyAge * 7} years old in dog years!`;
+}
+console.log(calculateDogAge(3));
+console.log(calculateDogAge(5));
+console.log(calculateDogAge(7));
