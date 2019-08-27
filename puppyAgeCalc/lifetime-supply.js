4beb89f963531a9e451d4c3feccf07b72cafcdb0
cocohost <kontakt.nicolas@gmail.com>
2019-08-27 14:02:57 +0200

    add bonus: rounding results

--- puppyAgeCalc/lifetime-supply.js
+++ puppyAgeCalc/lifetime-supply.js
@@ -1,9 +1,9 @@
 function calculateSupply(age, amountPerDay) {
   const maxAge = 90;
-  const supply = (maxAge - age) * amountPerDay * 365;
+  const supply = Math.floor((maxAge - age) * amountPerDay * 365);
   return `You will need ${supply} Snickers to last you until the ripe old age of ${maxAge}.`;
 }
 
-console.log(calculateSupply(52, 2));
-console.log(calculateSupply(31, 1));
-console.log(calculateSupply(89, 1));
+console.log(calculateSupply(52, 8.2));
+console.log(calculateSupply(31, 2.8));
+console.log(calculateSupply(89, 1.3));
